2d50212a255caebc8203b9892f250314be1beedb
Kathy Lee <lee.j.kathy@gmail.com>
2024-12-30 14:14:27 -0800

    clean

--- contents/src/tools/ToolFactory.java
+++
@@ -1,9 +0,0 @@
-package tools;
-
-/*
- * this is an interface for the Tool Concrete Factory class 
- * for the user to tell the program which object they want to initliaze 
- */
-public abstract class ToolFactory {
-	public abstract Tool createTool(String type); 
-}
